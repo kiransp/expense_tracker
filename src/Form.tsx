afd72bfa1d3003b547a62620267358c7773f7f08
kiran <spkiran.suresh@gmail.com>
2023-08-01 13:19:39 +0530

    working form with error handling.

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,34 +1,78 @@
+import { z } from "zod";
+import { FieldValues, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+// 1. creating a schema
+const schema = z.object({
+  amount: z.number({ invalid_type_error: "Amount is required" }),
+  category: z.string().nonempty({ message: "Category is required" }),
+  description: z
+    .string()
+    .min(3, { message: "Description should be minimum 3 characters" }),
+});
+
+// 2. extract the inferred type
+type FormData = z.infer<typeof schema>;
+
 function Form() {
+  // 3. Attach zod with react-form by passing the 2nd step type as <FormData>  and then passing a resolver
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = (data: FieldValues) => console.log(data);
   return (
     <div>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          console.log("Submitted");
-        }}
-      >
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className=" mb-3">
           <label htmlFor="description" className="form-label">
             Description
           </label>
-          <input id="description" type="text" className="form-control" />
+          <input
+            {...register("description", { required: true })}
+            id="description"
+            type="text"
+            className="form-control"
+          />
+          {errors.description && (
+            <p className="text-danger">{errors.description.message}</p>
+          )}
         </div>
         <div className="mb-3">
           <label htmlFor="amount" className="form-label">
             Amount
           </label>
-          <input id="amount" type="number" className="form-control" />
+          <input
+            {...register("amount", { required: true, valueAsNumber: true })}
+            id="amount"
+            type="number"
+            className="form-control"
+          />
+          {errors.amount && (
+            <p className="text-danger">{errors.amount.message}</p>
+          )}
         </div>
         <div className="mb-3">
           <label htmlFor="amount" className="form-label">
             Category
           </label>
-          <select id="category" className="form-select">
-            <option selected></option>
-            <option value="1">Groceries</option>
-            <option value="2">Utilities</option>
-            <option value="3">Entertainment</option>
+          <select
+            {...register("category", { required: true })}
+            id="category"
+            className="form-select"
+          >
+            <option></option>
+            <option value="groceries">Groceries</option>
+            <option value="utilities">Utilities</option>
+            <option value="entertainment">Entertainment</option>
           </select>
+          {errors.category && (
+            <p className="text-danger">{errors.category.message}</p>
+          )}
         </div>
         <button type="submit" className="btn btn-primary">
           Submit
